06c09200ab16749a11ccd158cb8fdd3159ec1b76
Millo Evers <exedysdigital@gmail.com>
2020-02-22 00:25:00 +0000

    Create datalogger.py

---
+++ datalogger.py
@@ -0,0 +1,12 @@
+from sense_hat import SenseHat
+from datetime import datetime
+
+sense = SenseHat()
+
+def get_sense_data():
+	sense_data = []
+  
+datetime.now()
+
+while True:
+	print(get_sense_data())
